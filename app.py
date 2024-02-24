35556d8a2a8a0c7d91d2a5397131247c51255374
barrettbeatrice <151100165+barrettbeatrice@users.noreply.github.com>
2024-02-24 17:48:15 -0600

    Final Final

--- app.py
+++ app.py
@@ -30,15 +30,12 @@ Base = automap_base()
 # Use the Base class to reflect the database tables
 Base.prepare(engine, reflect=True)
 
-
 # Assign the measurement class to a variable called `Measurement` and
 # the station class to a variable called `Station`
 Measurement = Base.classes.measurement
 Station = Base.classes.station
 
 
-# Create a session
-session = Session(engine)
 
 #################################################
 # Flask Setup
@@ -53,99 +50,166 @@ app = Flask(__name__)
 
 #Welcome Route
 @app.route("/")
-
 #Routing information added
 def welcome():
+	#List all available api routes
 	return(
-	'''
-	f"Welcome to Climate Analysis API!<br/>"
-    f"Available Routes:<br/>"
-    f"/api/v1.0/precipitation<br/>"
-    f"/api/v1.0/stations<br/>"
-    f"/api/v1.0/tobs<br/>"
-	f"/api/v1.0/temp/start/end"
-	''')
+		f"Welcome to Climate Analysis API!<br/>"
+    	f"Available Routes:<br/>"
+    	f"/api/v1.0/precipitation<br/>"
+    	f"/api/v1.0/stations<br/>"
+    	f"/api/v1.0/tobs<br/>"
+		f"/api/v1.0/temp/start<br/>"
+		f"/api/v1.0/temp/start/end"
+	)
 
 
 #<br/> tags and f string formatting in this context is for formatting HTML content on separate lines
 
-#Precipitation Route
+#Precipitation Route###################
 @app.route("/api/v1.0/precipitation")
-
 def precipitation():
-	# Calculate the date one year ago from the most recent date
-	prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
 
-	# Query: get date and precipitation for prev_year
-	precipitation = session.query(Measurement.date,Measurement.prcp).\
-		filter(Measurement.date >= prev_year).all()
-		
-	# Create dictionary w/ jsonify()--format results into .JSON
-	precip = {date: prcp for date, prcp in precipitation}
-	return jsonify(precip)
+    """Return a list of all precipitation and date"""
+    # Query all precipitation and date
+    results = session.query(Measurement.date,Measurement.prcp).all()
+
+    session.close()
+
+    # Convert list of tuples into dictionary
+    all_precepitation=[]
+    for date,prcp in results:
+        precipitation_dict = {}
+        precipitation_dict[date] = prcp
+        all_precepitation.append(precipitation_dict)
+
+    return jsonify(all_precepitation)
 
 
-
-#Stations Route
+#Stations Route#########################
 @app.route("/api/v1.0/stations")
-
 def stations():
-	results = session.query(Station.station).all()
-	
-	# Unravel results
-	stations = list(np.ravel(results))
-	return jsonify(stations=stations) 
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of all stations"""
+    # Query all stations
+    results = session.query(Station.id,Station.station,Station.name,Station.latitude,Station.longitude,Station.elevation).all()
+    # Close Section
+	session.close()
+    
+	#Fill the list made (including all info for each station)
+    #Append it with dictionary, jsonify it
+	all_station=[]
+    for id,station,name,latitude,longitude,elevation in results:
+        station_dict={}
+        station_dict['Id']=id
+        station_dict['station']=station
+        station_dict['name']=name
+        station_dict['latitude']=latitude
+        station_dict['longitude']=longitude
+        station_dict['elevation']=elevation
+        all_station.append(station_dict)
+    return jsonify(all_station)
+
 
 
 # Tobs Route
-app.route("/api/v1.0/tobs")
-def temp_monthly():
-    prev_year = dt.date(2017,8,23) - dt.timedelta(days=365)
-    results = session.query(Measurement.tobs).\
-        filter(Measurement.station == 'USC00519281').\
-        filter(Measurement.date >= prev_year).all()
-    temps = list(np.ravel(results))
-    return jsonify(temps=temps)
+@app.route("/api/v1.0/tobs")
+def tempartureobs():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
 
+    """Return a list of all temparture observations"""
+    
+    # Calculate the date 1 year ago from the last data point in the database
+    results_date=session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+    str_date=list(np.ravel(results_date))[0]
+    
+    latest_date=dt.datetime.strptime(str_date,"%Y-%m-%d")
+    
+    year_back=latest_date-dt.timedelta(days=366)  #Make the last year a variable, converted with dt
+    
+# Perform a query to retrieve the data and precipitation scores
+    results=session.query(Measurement.date, Measurement.tobs).order_by(Measurement.date.desc()).\
+            filter(Measurement.date>=year_back).all()
+    
+    session.close()
+    
+	#Make a list and populate, append dictionary
+    all_temperature=[]
+    for tobs,date in results:
+        tobs_dict={}
+        tobs_dict['date']=date
+        tobs_dict['tobs']=tobs
+        all_temperature.append(tobs_dict)
+    return jsonify(all_temperature)
+
+
+# Converting into proper Date-Times Format
+# Use `calc_temps` -- accept start date and end date in the format '%Y-%m-%d' 
+# and return the minimum, average, and maximum temperatures for that range of dates
+@app.route("/api/v1.0/<start>/<end>")
+def calc_temps(start, end):
+    
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+    
+	#Content for page, Arguments:
+    """TMIN, TAVG, and TMAX for a list of dates.
+    
+    Args:
+        start_date (string): A date string in the format %Y-%m-%d
+        end_date (string): A date string in the format %Y-%m-%d
+        
+    Returns:
+        TMIN, TAVE, and TMAX
+    """
+    #Query
+    results=session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+                filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+    
+	session.close()
+    
+	#Create Dictionary
+	temp_obs={}
+    temp_obs["Min_Temp"]=results[0][0]
+    temp_obs["avg_Temp"]=results[0][1]
+    temp_obs["max_Temp"]=results[0][2]
+    return jsonify(temp_obs)
 
 
 #Statistics Route
-# Provide both start and end date routes:
-@app.route("/api/v1.0/temp/<start>")
-@app.route("/api/v1.0/temp/<start>/<end>")
+# Provide start dates for temperature observations
+@app.route("/api/v1.0/<start>")
+def calc_temps_sd(start):
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+    
+    """TMIN, TAVG, and TMAX for a list of dates.
+    
+    Args:
+        start_date (string): A date string in the format %Y-%m-%d
+        
+    Returns:
+        TMIN, TAVE, and TMAX
+    """
+    
+	#Query
+    results=session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+                filter(Measurement.date >= start).all()
+    session.close()
+    
+	#Create Dictionary for Temp Statistics
+	temp_obs={}
+    temp_obs["Min_Temp"]=results[0][0]
+    temp_obs["avg_Temp"]=results[0][1]
+    temp_obs["max_Temp"]=results[0][2]
+    return jsonify(temp_obs)
 
-def stats(start='2017-06-01',end='2017-06-30'):
-	# Query: min, avg, max temps; create list called `sel`, if not given start or end date
-	#We know that in this case, end date is 08/23/2017
-	
-	sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
-
-	# Add `if-not` statement to determine if no start/end date given
-	if not end:
-		results = session.query(*sel).\
-			filter(Measurement.date >= start).\
-			filter(Measurement.date <= end).all()
-		temps = list(np.ravel(results))
-	    return jsonify(temps=temps)
-
-		#(*sel) - asterik indicates multiple results from query: minimum, average, and maximum temperatures
-
-	# Query: Calc statistics data
-	results = session.query(*sel).\
-		filter(Measurement.date >= start).\
-		filter(Measurement.date <= end).all()
-	temps = list(np.ravel(results))
-	return jsonify(temps=temps)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
-
-    #THE Statistical OUTPUT returns
-	# NOTE: Add following to path to address in browser:
-		# /api/v1.0/temp/2017-06-01/2017-06-30
-		# result: ["temps":[71.0,77.21989528795811,83.0]]
